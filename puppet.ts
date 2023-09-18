a577c3fa6174a2717986c880264282e842b6bf89
Mehran Mishekaran <aldeonmoriak@gmail.com>
2023-09-18 19:38:31 +0330

    fix timeout bug

--- puppet.ts
+++ puppet.ts
@@ -5,6 +5,8 @@ if (BROWSERLESS_TOKEN === undefined) {
   throw new TypeError("Missing BROWSERLESS_TOKEN environment variable.");
 }
 
+let isNotClicked = true;
+
 export async function getScreenshot(word: string) {
   const browser = await puppeteer.connect({
     browserWSEndpoint: `wss://chrome.browserless.io?token=${BROWSERLESS_TOKEN}`,
@@ -14,12 +16,14 @@ export async function getScreenshot(word: string) {
     await page.goto(`https://dictionnaire.lerobert.com/definition/${word}`, {
       waitUntil: "domcontentloaded",
     });
-    // INFO: if you have used browserless.io token before no need for this commented lines
-    // const closeButton = await page.waitForSelector(
-    //   "#onetrust-close-btn-container > button",
-    //   { timeout: 0 },
-    // );
-    // await closeButton?.click();
+    if (isNotClicked) {
+      const closeButton = await page.waitForSelector(
+        "#onetrust-close-btn-container > button",
+        { timeout: 0 },
+      );
+      await closeButton?.click();
+      isNotClicked = false;
+    }
     const main = await page.waitForSelector("main");
     const image = await main!.screenshot() as Uint8Array;
     return image;
